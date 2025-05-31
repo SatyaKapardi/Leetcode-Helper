42d63bf6fe7b9e5cf7b24666875e6012ad5bca22
Srisatya Kapardi Budi <94280881+SatyaKapardi@users.noreply.github.com>
2025-05-31 10:14:38 +0530

    Create vite.config.pages.js

---
+++ vite.config.pages.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": resolve("client/src"),
+      "@shared": resolve("shared"),
+      "@assets": resolve("attached_assets"),
+    },
+  },
+  root: "client",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
+});
